48400eecc75f1ae693a0e265b613085f5043c3a5
tkn24 <tkn24@cornell.edu>
2025-12-09 16:08:47 -0500

    updates to constraints

--- analyst.py
+++ analyst.py
@@ -1,5 +1,6 @@
 from union_find import UnionFind
 
+
 class TypeNode:
     def __init__(self, uf_id):
         # Union-Find ID
@@ -16,6 +17,11 @@ class TypeNode:
         self.lam_rets = None
 
 
+# Helpful information for following from Steensgaard's paper:
+# every time Steensgaard calls type(e) we use self.nodes[e]
+# where e is a UF ID
+
+
 class Analyst:
     def __init__(self):
         # The Union-Find instance used for managing Types
@@ -26,6 +32,16 @@ class Analyst:
 
         # A mapping of UF IDs to sets of pending UF IDs
         self.pending = {}
+
+    # Helper function for finding ECR representatives
+    def rep(self, x):
+        if isinstance(x, TypeNode):
+            x = x.uf_id
+        return self.uf.find(x)
+
+    # Helper function: get TypeNode for an id or TypeNode
+    def node_for(self, x):
+        return self.nodes[self.rep(x)]
 
     # Instantiation of a new Type node
     def new_type(self, uf_id):
@@ -38,15 +54,18 @@ class Analyst:
         # Assume ID is variable name
         self.nodes[uf_id] = node
 
+        # Initialize pending set for new ID
+        self.pending[uf_id] = set()
+
         return node
-    
 
     def join(self, e1, e2):
-        t1 = self.nodes[e1]
-        t2 = self.nodes[e2]
+        t1 = self.node_for(e1)
+        t2 = self.node_for(e2)
 
+        # Union e1 and e2
         self.uf.union(e1, e2)
-        e = self.uf.find(e1) # Or return from union()
+        e = self.uf.find(e1)  # Or return from union()
 
         if t1.is_bottom:
             self.nodes[e] = t2
@@ -55,44 +74,77 @@ class Analyst:
                 self.pending[e] = self.pending[e1] | self.pending[e2]
             else:
                 for x in self.pending[e1]:
-                    self.join(e1, x)
+                    self.join(e, x)
 
         else:
             self.nodes[e] = t1
 
             if t2.is_bottom:
-                for x in self.pending[e1]:
-                    self.join(e1, x)
+                for x in self.pending[e2]:
+                    self.join(e, x)
             else:
                 self.unify(t1, t2)
-    
 
     def unify(self, t1, t2):
         if t1.tau != t2.tau:
-            self.join(t1.uf_id, t2.uf_id)
+            self.join(t1.tau, t2.tau)
 
-        if len(t1.lam_args) != len(t2.lam_args) or len(t1.lam_rets) != len(t2.lam_rets):
-            print("ERROR")
-            return
-        
-        for i in range(len(t1.lam_args)):
-            self.join(t1.lam_args[i].uf_id, t2.lam_args[i].uf_id)
+        # if len(t1.lam_args) != len(t2.lam_args) or len(t1.lam_rets) != len(t2.lam_rets):
+        #     print("ERROR")
+        #     # case with different arity (should not happen)
+        #     return
 
-        for i in range(len(t1.lam_rets)):
-            self.join(t1.lam_rets[i].uf_id, t2.lam_rets[i].uf_id)
+        # for i in range(len(t1.lam_args)):
+        #     self.join(t1.lam_args[i].uf_id, t2.lam_args[i].uf_id)
 
+        # for i in range(len(t1.lam_rets)):
+        #     self.join(t1.lam_rets[i].uf_id, t2.lam_rets[i].uf_id)
 
     def cjoin(self, e1, e2):
         t2 = self.nodes[e2]
 
         if t2.is_bottom:
-            self.pending[e2] = e1 | self.pending[e2]
+            self.pending[e2] = {e1} | self.pending[e2]
         else:
             self.join(e1, e2)
-
 
     def settype(self, e, t):
         self.nodes[e] = t
 
         for x in self.pending[e]:
             self.join(e, x)
+
+    def handle_assign(self, x, y):
+        # handle the assignment x := y
+        e1 = self.rep(x)
+        e2 = self.rep(y)
+        t1 = self.nodes[e1]
+        t2 = self.nodes[e2]
+
+        print("Handling assign:", x, y)
+        print("Types before:", t1, t2)
+        print("Taus before:", t1.tau, t2.tau)
+
+        if t1.tau != t2.tau:
+            self.cjoin(t1.tau, t2.tau)
+
+    def handle_addr_of(self, x, y):
+        # handle the assignment x := &y
+        print("Handling address of:", x, y)
+
+        e1 = self.rep(x)
+        e2 = self.rep(y)
+        t1 = self.nodes[e1]
+
+        # If t1 is ⊥, set its type to point to e2 (and make it non-bottom)
+        if t1.is_bottom:
+            t1.tau = e2
+            t1.is_bottom = False
+            # Satisfy any pending assignments on e1
+            self.settype(e1, t1)
+
+        print("EQR:", e1, e2)
+        print("Taus before:", t1.tau, e2)
+
+        if t1.tau != e2:
+            self.join(e1, e2)
--- steensgaard.py
+++ steensgaard.py
@@ -11,8 +11,9 @@ import argparse
 import sys
 import actions
 from sil_parser import *
-from type import TypeManager
 from union_find import UnionFind
+from analyst import *
+from type import TypeManager
 
 
 def parse_program(program: str):
@@ -43,16 +44,16 @@ def create_graph(uf, map):
         for node in set:
             G.add_node(tuple(set))
     for key, node in map.items():
-        if node.tau_ref is None:
+        if node.tau is None:
             continue
         else:
             # Find the set that contains node.uf_id
             start = "start"
             end = "end"
-            if node.tau_ref is None:
+            if node.tau is None:
                 continue
             else:
-                end_node = node.tau_ref.uf_id
+                end_node = node.tau
                 for set in sets:
                     if end_node in set:
                         end = tuple(set)
@@ -76,7 +77,7 @@ def create_graph(uf, map):
     return G
 
 
-def main(args=None):
+def main_old(args=None):
     """
     The main entry point to start the analysis.
     Takes in command-line arguments for the filename of a SIL program.
@@ -154,6 +155,80 @@ def main(args=None):
                     print(key, node.uf_id, node.tau_ref, "<--")
 
             G = create_graph(manager.uf, map)
+            return 0
+    else:
+        print("No filename provided.")
+        return -1
+
+
+def main(args=None):
+    """
+    The main entry point to start the analysis.
+    Takes in command-line arguments for the filename of a SIL program.
+    Returns the completed analysis.
+    """
+
+    parser = argparse.ArgumentParser(
+        description="A python implementation of Steensgaard's Points-to Analysis."
+    )
+
+    # Command line arguments
+    parser.add_argument(
+        "-fn", "--filename", type=str, help="Specify a filename for a SIL program."
+    )
+
+    # Parse the arguments
+    # If args is None, parse_args will default to sys.argv[1:]
+    program_fn = parser.parse_args(args).filename
+
+    # Access the parsed arguments
+    if program_fn:
+        with open(program_fn, "r") as f:
+            program = f.read()
+            print(program)
+            ast, constraints = parse_program(program)
+            print("ALL CONSTRAINTS:")
+            print(constraints)
+            print("Parsing completed.")
+
+            analyst = Analyst()
+
+            for c in constraints:
+                print("Processing constraint:", c)
+
+                # Find Type node of LHS variable
+                x = analyst.nodes.get(c["lhs"])
+
+                # Instantiate Type if LHS variable is new
+                if x is None:
+                    x = analyst.new_type(c["lhs"])
+
+                # Accommodate operations with no RHS variable
+                y = None
+
+                # Find type node of RHS variable
+                if "rhs" in c:
+                    y = analyst.nodes.get(c["rhs"])
+
+                    # Instantiate Type node if RHS variable is new
+                    if y is None:
+                        y = analyst.new_type(c["rhs"])
+
+                match c["type"]:
+                    case "assign":
+                        print("assign", c["lhs"], c["rhs"])
+                        analyst.handle_assign(c["lhs"], c["rhs"])
+                    case "addr_of":
+                        print("addr_of", c["lhs"], c["rhs"])
+                        analyst.handle_addr_of(c["lhs"], c["rhs"])
+                    case _:
+                        print("Unrecognized constraint.")
+                print(analyst.uf)
+                print(analyst.nodes)
+                for key, node in analyst.nodes.items():
+                    print(key, node.uf_id, node.tau, "<--")
+
+            G = create_graph(analyst.uf, analyst.nodes)
             return 0
     else:
         print("No filename provided.")
